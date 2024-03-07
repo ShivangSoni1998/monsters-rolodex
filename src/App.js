7441aa93cee8844fe058b151157e4476c770a569
Shivang Soni <shivangsoni1998@gmail.com>
2024-03-07 17:40:15 -0500

    fix unlimited fetching of monsters and rendering of filtered monsters by using useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
@@ -9,24 +9,39 @@ import React from 'react';
 const App = () => {
   const [monsters, setMonsters] = useState([]);
   const [searchField, setSearchField] = useState('');
+  const [title, setTitle] = useState('');
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => setMonsters(users));
-  });
+  }, []);
 
   const onSearchChange = (e) => {
     setSearchField(e.target.value);
   };
 
-  const filteredMonsters = monsters.filter((monster, idx) =>
-    monster.name.toLowerCase().includes(searchField.toLowerCase())
+  const onTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const filteredMonsters = useMemo(
+    () =>
+      monsters.filter((monster, idx) => {
+        console.log('render');
+        return monster.name.toLowerCase().includes(searchField.toLowerCase());
+      }),
+    [searchField, monsters]
   );
 
   return (
     <div className='App'>
-      <h1 className='app-title'>Monsters Rolodex</h1>
+      <h1 className='app-title'>{title}</h1>
+      <SearchBox
+        onSearchChange={onTitleChange}
+        placeholder='search title'
+        className='monsters-search-box'
+      />
       <SearchBox
         onSearchChange={onSearchChange}
         placeholder='search monsters'
